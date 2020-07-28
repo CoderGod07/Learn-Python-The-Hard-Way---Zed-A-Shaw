6f68b1660c9e7cbcda455b39de166a03a7040201
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-28 18:33:43 +0530

    initial commit

---
+++ ex3.py
@@ -0,0 +1,28 @@
+# prints a statement
+print("I will now count my chickens:")
+# prints the number of hens
+print("Hens", 25 + 30 / 6)
+# prints the number of roosters
+print("Roosters", 100 -25 * 3 % 4)
+# prints a statement
+print("Now i will count the eggs:")
+#  prints the number of eggs
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# prints if a statement is true
+print("It is true that 3 + 2 < 5 - 7?")
+# prints solution of an expression
+print(3 + 2 < 5 - 7)
+# prints if its true
+print("what is 3 + 2?", 3 + 2)
+# prints if its true
+print("what is 5 - 7?", 5 - 7)
+# prints a statement
+print("Oh that's why it's False.")
+# prints a statement
+print("How about some more.")
+# prints if a statemtn is true
+print("Is it greater?", 5 > -2)
+# prints if statement is true
+print("Is it greater or equal?", 5 >= -2)
+# prints if a statment is true
+print("Is it less or equal?" , 5 <= -2)
