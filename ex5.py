140e2311328e5371087bc5f733bafca88f695976
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-29 20:34:02 +0530

    initial commit

---
+++ ex5.py
@@ -0,0 +1,19 @@
+name = "Kush"
+age = 22
+height = 174 #cms
+InchestoCms = 
+weight = 72 #kgs
+eyes = "brown"
+teeth = 'white'
+hair = 'brown'
+
+print(f"Let's talk about {name}")
+print(f"He's {height} cms tall")
+print(f"He's {weight} kgs heavy")
+print("Actually that's not too heavy")
+print(f"He's got {eyes} eyes and {hair} hair")
+print(f"He's teeth are usually {teeth} depending on the coffee")
+
+# this line is tricky, try to get it exactly right
+total = height + weight + age
+print(f"If I add {height}, {age}, and {weight}, I get {total}")
