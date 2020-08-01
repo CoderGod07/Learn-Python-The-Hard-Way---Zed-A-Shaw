241d571579d6c020a330dddea29079fee85bbbf5
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-01 13:17:01 +0530

    adding comment and use of end method

--- ex7.py
+++ ex7.py
@@ -1,9 +1,13 @@
+#printing a string
 print("Mary had a little lamb.")
+# printing a string using formatting
 print("It's fleece was white as {}.format('snow')")
+# printing a string 
 print("And everywhere that marry went.")
+# printing a string multiplied  10 times
 print("." * 10) #multiplies the string 10 times hence 10 dots
 
-end1 = "C"
+end1 = "C" #storing a string in a variable
 end2 = "h"
 end3 = "e"
 end4 = "e"
@@ -17,5 +21,6 @@ end11 = "e"
 end12 = "r"
 
 #watch 
-print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ') 
+# string concatenation
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ') # end is used to end a print line with the argument provided in end
 print(end7 + end8 + end9 + end10 + end11 + end12)
