e06c2154f1303d4242870b682827c80a4ba35ed6
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-02 14:03:41 +0530

    initial commit NEW LINE METHOD

---
+++ ex9.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thurs Fri Sat Sun"
+month = "Jan\nFeb\nMar\nApr\nMay\nJune\nAug"
+
+print("These are the days: ", days)
+print("Here are the months: ", month)
+print("""                             
+There's something going on here
+Line 1
+Line 2
+Line 4
+""")
