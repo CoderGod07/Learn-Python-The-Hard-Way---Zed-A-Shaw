5c9ae8b4adfc7cf8030bc09d781bf976cf5ae2d7
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-09-13 14:09:52 +0530

    SD python -m pydoc

--- ex12.py
+++ ex12.py
@@ -3,3 +3,10 @@ height = input("How tall are you?: ")
 weight = input("How much do you weigh?: ")
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+
+# SD
+
+# use python -m pydoc ___  to read the documentation of a module function class etc. when not in python 
+# q to exit 
+# use help(__)   when in python to the same
