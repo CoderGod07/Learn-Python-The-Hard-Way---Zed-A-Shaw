483a951e9810e247dddd19c3823d391f6fdba09f
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-10-07 18:52:45 +0530

    final code

--- ex14.py
+++ ex14.py
@@ -1,10 +1,9 @@
 from sys import argv
 
 script, user_name = argv
+prompt = '> '      # handy to use so that we dont have to type everytime we use an input
 
-prompt = '> '
-
-print(f"Hi {user_name}, I'm the {script}")
+print(f"Hi {user_name}, I'm the {script} script")
 print("I'd like to ask you a few questions")
 print(f"Do you like me {user_name}?")
 likes = input(prompt)
@@ -12,4 +11,11 @@ likes = input(prompt)
 print(f"Where do you live {user_name}?")
 lives = input(prompt)
 
-print("What kind of computer have?")
+print("What kind of computer fo you have?")
+computer = input(prompt)
+
+print(f"""
+Alright so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
+""")
