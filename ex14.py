a1a4c41c56a062f062395157242e084b1bc87d64
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-10-07 18:55:42 +0530

    sd

--- ex14.py
+++ ex14.py
@@ -1,7 +1,7 @@
 from sys import argv
 
-script, user_name = argv
-prompt = '> '      # handy to use so that we dont have to type everytime we use an input
+script, user_name, check = argv
+prompt = 'wazz goood '      # handy to use so that we dont have to type everytime we use an input
 
 print(f"Hi {user_name}, I'm the {script} script")
 print("I'd like to ask you a few questions")
@@ -19,3 +19,4 @@ Alright so you said {likes} about liking me.
 You live in {lives}. Not sure where that is.
 And you have a {computer} computer. Nice.
 """)
+print(f"This was test number {check}.")
