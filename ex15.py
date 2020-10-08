3ce7068ef6fd50f904e4cf316c0de492408aa059
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-10-08 16:13:50 +0530

    initial commit

--- ex15.py
+++ ex15.py
@@ -4,10 +4,12 @@ script, filename = argv
 
 txt = open(filename)
 
-print(f"Here's your file{filename}:")
+print(f"Here's your file {filename}:")
 print(txt.read())
 
 print("Type the filename again:")
 file_again = input("> ")
 
-txt_ag
+txt_again = open(file_again)
+
+print(txt_again.read())
